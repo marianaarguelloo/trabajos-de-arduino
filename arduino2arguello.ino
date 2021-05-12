180d8b2471f58c590c9711c9c0af5334c73f9e66
topote14 <44424305+topote14@users.noreply.github.com>
2021-05-12 13:02:20 -0300

    Update arduino2arguello.ino

--- arduino2arguello.ino
+++ arduino2arguello.ino
@@ -1,13 +1,12 @@
-int temperatura = 0
-
 void setup() {
-  pinMode (A5,INPUT);
-  Serial.begin (9600);
+  pinMode (A5,INPUT); //define que usa el pin A5 como entrada
+  Serial.begin (9600); //indico que voy a usar el puerto serial a 9600 baudios
 }
 
 void loop() {
-  temperatura = -40 + 0.488155 * (analogRead(A5) - 20); //leo la temperatura en grados C
+  int temperatura = -40 + 0.488155 * (analogRead(A5) - 20); //leo la temperatura en grados C
   delay (1000);
-  Serial.print (" La temperatura es");
-
+  Serial.print (" La temperatura es ");
+  Serial.print(temperatura);
+  Serial.println(" grados.");
 }
